a3008e3c2dd6275ce255f8d8b2ddc66041a4b7dc
c <132262468+imautisticbro@users.noreply.github.com>
2023-10-07 14:25:01 +0600

    solution for task with while
    Develop a program that displays a line of symbols on the screen. The number of characters, what character to use, and what line will be - vertical or horizontal - is specified by the user

---
+++ cycles.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	int symbolcount;
+	char symbol;
+	int linetype;
+	int index = 0;
+
+	cout << "enter quantity of symbols" << endl;
+	cin >> symbolcount;
+
+	cout << "enter symbol" << endl;
+	cin >> symbol;
+
+	cout << "choose type of the line" << endl
+		<< "1. vertical" << endl
+		<< "2. horizontal" << endl;
+	cin >> linetype;
+
+	while (index < symbolcount)
+	{
+		switch (linetype)
+		{
+		case 1:
+			cout << symbol << endl;
+			index++;
+			break;
+		case 2:
+			cout << symbol;
+			index++;
+			break;
+		default:
+			cout << "wrong number" << endl;
+			index = symbolcount;
+			break;
+		}
+	}
+	cout << endl << endl;
+}
