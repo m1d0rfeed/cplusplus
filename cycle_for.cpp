b22a453c55addb18541d574ca2a41b8c32f45628
c <132262468+imautisticbro@users.noreply.github.com>
2023-10-07 17:15:31 +0600

    solution task creating rectangle
    create a programme which displays rectangle with symbol *, width and height are specified by the user

---
+++ cycle_for.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	int height, width;
+
+	cout << "enter height" << endl; 
+	cin >> height;
+
+	cout << "enter width" << endl;
+	cin >> width;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+		
+	}
+
+
+}
